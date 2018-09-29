990264f440db272528a7ab415af9ca8174866546
Tino Gutiérrez <tgf1987@gmail.com>
2018-09-29 19:11:29 +0200

    FormValidator updated

--- superEjercicio/src/libs/FormValidator.class.js
+++ superEjercicio/src/libs/FormValidator.class.js
@@ -10,16 +10,16 @@ export class FormValidator {
 		this._form.addEventListener('submit', this._onSubmit.bind(this), true);
 		this._form.addEventListener("invalid", this._eachInvalidFieldonSubmit.bind(this), true);
 
-		this._invalidCounter = 0;	
+		this._invalidCounter = 0;
 		this._errors = [];
 	}
 
 	_markInvalidfields(fields) { 
 		
-		inputs.forEach((field) => input.classList.add('input-error'));
+		fields.forEach((field) => field.classList.add('input-error'));
 	}
 
-	_unmarkInvalidField(form) { 
+	_unmarkInvalidFields(form) { 
 
 		for (let field of form) {
 		
@@ -31,7 +31,7 @@ export class FormValidator {
 	
 		event.preventDefault();
 		
-		this.__unmarkInvalidField(this._form);
+		this._unmarkInvalidFields(this._form);
 		
 		this._box.cleanAlertBox();
 		this._box.currentType = "success";
@@ -41,6 +41,8 @@ export class FormValidator {
 	_eachInvalidFieldonSubmit(event) {
 		
 		event.preventDefault();
+
+		console.log("//////////////////////");
 
 		let { 
 			currentTarget: form, 
@@ -66,32 +68,53 @@ export class FormValidator {
 		let invalidFields = form.querySelectorAll(':invalid'); 
 
 		this._errors.push(errorMessage);
-		
-		if (this._invalidCounter == 0) {
-			
-			this._errors = [];
 
-			if (typeof this._box === "undefined"){
-				
-				this._box = new AlertBox('error', 300);
-			} 		
+		if (this._box === undefined) {
+
+			console.log(this._box);
+			console.log("indefinido!");
+			this._box = new AlertBox('error', 300);
+			console.log("nueva box creada");
+
 		}
 
+		if (invalidFields.length > 0) { //creo que se puede quitar
+
+			this._box.addAlert(errorMessage);
+			this._box.show();	
+
+		}
+
+/*		if (this._invalidCounter == 0) {
+
+			console.log(this._invalidCounter);
+
+			this._errors = [];
+
+			if (typeof this._box === undefined){
+				
+				this._box = new AlertBox('error', 300);
+				this._invalidCounter++;
+				console.log(this._invalidCounter);
+				console.log(invalidFields.length);
+
+			} 		
+		} */
+
 		if(this._invalidCounter == invalidFields.length) {
-
 			
-			this.__unmarkInvalidField(form);
-			this.__markInvalidfields(invalidFields);
-
+			this._unmarkInvalidFields(form);
+			this._markInvalidfields(invalidFields);
 			this._box.cleanAlertBox();
 
-			this._errors.forEach((alert) => this._box.addAlert(alert));
+			//this._errors.forEach((alert) => this._box.addAlert(alert));
 			
-			this._box.show();
+			//this._box.show();
 			
 			this._invalidCounter = 0;
 		} else {
 			this._invalidCounter++;
+			console.log(this._invalidCounter);
 		}
 	}	
 }
