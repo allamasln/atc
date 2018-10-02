e561a87d2199a30e431b32018c59cd7f5e03111e
Antonio <antoniollamasln@gmail.com>
2018-10-02 16:20:00 +0200

    fixed

--- atc/superEjercicio/src/libs/FormValidator.class.js
+++ atc/superEjercicio/src/libs/FormValidator.class.js
@@ -90,7 +90,7 @@ export class FormValidator {
 		
 		} = invalidField;
 
-		let errorMessage = ` Error: ${ errorValidationMessage } (${ nameInvalidField }) ${ errorTitleMessage }.`;
+		let errorMessage = `Error: ${ errorValidationMessage } (${ nameInvalidField }) ${ errorTitleMessage }.`;
 
 		this._alerts.push(errorMessage);		
 			
